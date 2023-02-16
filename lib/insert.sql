53b65b58420d7b8d5b03cb4a7f3ad39bf0681681
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2023-02-16 18:50:05 +0300

    hands on with SQL queries

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 1, "M", "Black", "choleric", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 2, "M", "Brown", "sanguine", false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 3, "F", "Black", "melancholic", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 4, "F", "Brown", "choleric", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 6, "F", "Black", "melancholic", false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 7, "M", "White", "phlegmatic", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 8, "F", "Brown", "sanguinec", false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 5, "F", "Black", "phlegmatic", true);
